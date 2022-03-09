50bc77ea1313d8dfce20aeec7a10c6969f97a30e
Terrys101 <97230300+Terrys101@users.noreply.github.com>
2022-03-09 22:52:23 +0000

    Update user controller. list - user  delete -user

--- src/user/userRoutes.js
+++ src/user/userRoutes.js
@@ -1,11 +1,16 @@
 const { Router } = require("express");
-const { addUser, login } = require("./userControllers");
-const { hashPassword, decryptPassword, checkToken } = require("../middleware");
+const { addUser, login,updatePassword } = require("./userControllers");
+const {listUsers} = require("./userControllers");
+const { hashPassword, decryptPassword, checkToken} = require("../middleware");
+const {deleteUser} = require("./userControllers")
+const User = require("./userModel");
 const userRouter = Router();
 
 userRouter.post("/user", hashPassword, addUser);
+userRouter.get("/user", listUsers,)
 userRouter.post("/login", decryptPassword, login);
 userRouter.get("/user", checkToken,login);
-
+userRouter.patch("/user",hashPassword, checkToken,updatePassword );
+userRouter.delete("/user/:filterKey/:filterVal", deleteUser);
 
 module.exports = userRouter;
